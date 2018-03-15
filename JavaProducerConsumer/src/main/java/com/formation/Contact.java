6a92b21edd39762863b5ce5ef745e37ebc8a61fd
ctith <36633720+ctith@users.noreply.github.com>
2018-03-15 10:11:52 +0100

    JavaProducerConsumer

---
+++ JavaProducerConsumer/src/main/java/com/formation/Contact.java
@@ -0,0 +1,24 @@
+package com.formation;
+
+public class Contact {
+
+    private String name;
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    private String firstname;
+    public String getFirstname() { return firstname; }
+    public void setFirstname(String firstname) {this.firstname = firstname; }
+
+    private Integer age;
+    public Integer getAge() { return age;  }
+    public void setAge(Integer age) {  this.age = age;}
+
+    private String adresse;
+    public String getAdresse() {return adresse;}
+    public void setAdresse(String adresse) {this.adresse = adresse;}
+}
